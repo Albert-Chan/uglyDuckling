ed4394fe79418182210aca60292a095dcdc7a28d
yuchen <ychen@actuate.com>
2013-06-17 18:35:40 +0800

    Disable auto reload ignore db file

--- bbsEx/manage.py
+++ bbsEx/manage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import os
-import pydevd
+#import pydevd
 import sys
-pydevd.patch_django_autoreload(
-patch_remote_debugger=True, #Connect to the remote debugger.
-patch_show_console=True
-)
+#pydevd.patch_django_autoreload(
+#patch_remote_debugger=True, #Connect to the remote debugger.
+#patch_show_console=True
+#)
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bbsEx.settings")
 
