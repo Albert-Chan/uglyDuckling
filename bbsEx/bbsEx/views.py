507a6555b6ef740fc037a5ccdca255883e85c9de
azhu <azh716@gmail.com>
2013-06-20 14:37:16 +0800

    Fixed login redirect and hello page

--- bbsEx/bbsEx/views.py
+++ bbsEx/bbsEx/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
 def hello(request):
-    return render_to_response('hello.html')
+    return render_to_response('hello.html',  {'user': request.user, })
 
 def index(request):
     return render_to_response('index.html')
